49291acd7a92b5811042f5cf14f7e0d466b39cdf
DmitriyIvanko <dmitriy.ivanko@yumasoft.com>
2018-04-27 12:12:28 +0300

    + add capture photo;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
 
+import { WebCamComponent } from 'ack-angular-webcam';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public webcam: WebCamComponent;
+  public picture: string;
+
+  public getPicture() {
+    this.webcam.getBase64()
+      .then((base64) => {
+        this.picture = base64;
+      });
+  }
 }
