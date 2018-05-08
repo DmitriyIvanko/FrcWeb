6febc08b3b1281c93eccc2e2c5300681f937fc91
DmitriyIvanko <dmitriy.ivanko@yumasoft.com>
2018-05-08 22:50:10 +0300

    + add simple requests;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import {
+  Http,
+  Request,
+  Response,
+} from "@angular/http";
+import { Observable } from "rxjs/Observable";
 
 import { WebCamComponent } from 'ack-angular-webcam';
 
@@ -8,13 +14,30 @@ import { WebCamComponent } from 'ack-angular-webcam';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public webcam: WebCamComponent;
-  public picture: string;
+    public webcam: WebCamComponent;
+    public picture: string;
 
-  public getPicture() {
-    this.webcam.getBase64()
-      .then((base64) => {
-        this.picture = base64;
+    constructor(
+      private http: Http,
+    ) { }
+
+    public request(): Observable<any> {
+      const request = new Request({
+        method: "POST",
+        url: `http://localhost:1752/api/login/sign-in`,
       });
-  }
+
+      return this.http.request(request);
+    }
+
+    public getPicture() {
+      this.request().subscribe((result) => {
+        debugger;
+      })
+
+      this.webcam.getBase64()
+        .then((base64) => {
+          this.picture = base64;
+        });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { WebCamModule } from 'ack-angular-webcam';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     WebCamModule,
+    HttpModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
